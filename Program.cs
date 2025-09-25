6be193ceb68e35d088744dec01bff8acaec686df
vohaikhoa2005 <vohaikhoa2005@gmail.com>
2025-09-25 13:41:35 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Lab3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student();
+            Student student2 = new Student(99,"John", "Doe", new DateTime(2000, 12, 31));
+
+            Console.WriteLine("Student 1: ");
+            Console.WriteLine("ID: " + student1.StudentID);
+            Console.WriteLine("Name: " + student1.FirstName + " " + student1.LastName);
+            Console.WriteLine("DOB: " + (student1.DateOfBirth.HasValue ? student1.DateOfBirth.Value.ToShortDateString() : "N/A"));
+            Console.WriteLine("Enrollment Date: " + student1.EnrollmentDate.ToShortDateString());
+            Console.WriteLine("-------------------");
+            Console.WriteLine("Student 2: ");
+            Console.WriteLine("ID: " + student2.StudentID);
+            Console.WriteLine("Name: " + student2.FirstName + " " + student2.LastName);
+            Console.WriteLine("DOB: " + (student2.DateOfBirth.HasValue ? student2.DateOfBirth.Value.ToShortDateString() : "N/A"));
+            Console.WriteLine("Enrollment Date: " + student2.EnrollmentDate.ToShortDateString());
+            Console.WriteLine("-------------------");   
+            Console.WriteLine("Total Students: " + Student.TotalStudent);
+        }
+    }
+
+}
---
+++ Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public DateTime EnrollmentDate { get; set; } = DateTime.Now;
+        public static int TotalStudent { get; set; } = 0;
+
+        public Student()
+        {
+            StudentID = 0;
+            FirstName = "Michel";
+            LastName = "Ject";
+            DateOfBirth = null;
+            EnrollmentDate = DateTime.Now;
+            TotalStudent++;
+        }
+        public Student(string firstName, string lastName, DateTime? dateOfBirth = null)
+        {
+            StudentID = this.StudentID;
+            FirstName = firstName;
+            LastName = lastName;
+            DateOfBirth = dateOfBirth;
+            EnrollmentDate = DateTime.Now;
+            TotalStudent++;
+        }
+    }
+}
